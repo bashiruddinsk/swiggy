5619ce1a7f5b263f3f003201a60eae85729980f6
shaik bashiruddin <bashiruddin.sk4664@gmail.com>
2024-08-15 11:03:01 +0530

    latest changes

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,39 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.css']
-// })
-// export class RegisterComponent {
-//   registerForm: FormGroup;
-
-//   constructor(private fb: FormBuilder, private router: Router) {
-//     // Initialize the form with validators
-//     this.registerForm = this.fb.group({
-//       username: ['', Validators.required],
-//       password: ['', Validators.required]
-//     });
-//   }
-
-//   onRegister() {
-//     if (this.registerForm.valid) {
-//       const { username, password } = this.registerForm.value;
-      
-//       // Store the user data in localStorage (for demo purposes)
-//       localStorage.setItem('user', JSON.stringify({ username, password }));
-
-//       alert('Registration successful! Please login.');
-//       this.router.navigate(['/login']);  // Redirect to the login page
-//     } else {
-//       // If form is invalid, show validation errors
-//       this.registerForm.markAllAsTouched();
-//     }
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -71,32 +35,3 @@ export class RegisterComponent {
   }
 }
 
-
-// import { Component } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.css']
-// })
-// export class RegisterComponent {
-//   registerForm: FormGroup;
-
-//   constructor(private fb: FormBuilder, private router: Router) {
-//     this.registerForm = this.fb.group({
-//       username: ['', Validators.required],
-//       email: ['', [Validators.required, Validators.email]],
-//       password: ['', Validators.required]
-//     });
-//   }
-
-//   onRegister(): void {
-//     if (this.registerForm.valid) {
-//       // Perform registration logic
-//       // After registration, navigate back to login
-//       this.router.navigate(['/login']);
-//     }
-//   }
-// }
